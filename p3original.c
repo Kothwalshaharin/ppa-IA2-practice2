da5bd4c9a7c374c7910bc9652fd0d033c7f9b6d2
shaharin k <93109454+Kothwalshaharin@users.noreply.github.com>
2022-02-11 09:35:09 +0000

    p3original

--- p3original.c
+++ p3original.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
 int input_number()
 {
   int n;
   printf("Enter the number\n");
-  scanf"%d" , &n);
+  scanf("%d",&n);
   return n;
 }
-
 int is_composite(int n)
 {
-  for(int i=2; i<n/2; i++
+  for(int i = 2; i < n/2; i++)
   {
-    if(n%1 ==0)
-    return 0;
+    if(n%i == 0)
+    return 1;
   }
+  return 0;
+}
